d644c11f6ef61f36506a43929e7bc0b47f56f593
Chris Coffee <chrico@uab.edu>
2023-03-23 11:18:26 -0500

    cleaned up code

--- gower/gower_dist.py
+++ gower/gower_dist.py
@@ -1,7 +1,7 @@
-from scipy.sparse import issparse
-import numpy as np
-import pandas as pd
 from functools import partial
+
+import numpy as np
+from scipy.sparse import issparse
 from tqdm.contrib.concurrent import process_map
 
 
@@ -19,19 +19,20 @@ def f(i, x_n_rows, y_n_rows, X_cat, X_num, Y_cat, Y_num,
                     weight_num,
                     weight_sum,
                     num_ranges)
-    # print(res)
     return res
 
-def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):
 
+def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):
     # function checks
     X = data_x
-    if data_y is None: Y = data_x
-    else: Y = data_y
+    if data_y is None:
+        Y = data_x
+    else:
+        Y = data_y
     if not isinstance(X, np.ndarray):
         if not np.array_equal(X.columns, Y.columns): raise TypeError("X and Y must have same columns!")
     else:
-         if not X.shape[1] == Y.shape[1]: raise TypeError("X and Y must have same y-dim!")
+        if not X.shape[1] == Y.shape[1]: raise TypeError("X and Y must have same y-dim!")
 
     if issparse(X) or issparse(Y): raise TypeError("Sparse matrices are not supported!")
 
@@ -46,21 +47,19 @@ def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):
             cat_features = np.zeros(x_n_cols, dtype=bool)
             for col in range(x_n_cols):
                 if not np.issubdtype(type(X[0, col]), np.number):
-                    cat_features[col]=True
+                    cat_features[col] = True
     else:
         cat_features = np.array(cat_features)
-
-    # print(cat_features)
 
     if not isinstance(X, np.ndarray): X = np.asarray(X)
     if not isinstance(Y, np.ndarray): Y = np.asarray(Y)
 
-    Z = np.concatenate((X,Y))
+    Z = np.concatenate((X, Y))
 
-    x_index = range(0,x_n_rows)
-    y_index = range(x_n_rows,x_n_rows+y_n_rows)
+    x_index = range(0, x_n_rows)
+    y_index = range(x_n_rows, x_n_rows + y_n_rows)
 
-    Z_num = Z[:,np.logical_not(cat_features)]
+    Z_num = Z[:, np.logical_not(cat_features)]
 
     num_cols = Z_num.shape[1]
     num_ranges = np.zeros(num_cols)
@@ -79,16 +78,14 @@ def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):
         num_ranges[col] = np.abs(1 - min / max) if (max != 0) else 0.0
 
     # This is to normalize the numeric values between 0 and 1.
-    Z_num = np.divide(Z_num ,num_max,out=np.zeros_like(Z_num), where=num_max!=0)
-    Z_cat = Z[:,cat_features]
+    Z_num = np.divide(Z_num, num_max, out=np.zeros_like(Z_num), where=num_max != 0)
+    Z_cat = Z[:, cat_features]
 
     if weight is None:
         weight = np.ones(Z.shape[1])
 
-    #print(weight)
-
-    weight_cat=weight[cat_features]
-    weight_num=weight[np.logical_not(cat_features)]
+    weight_cat = weight[cat_features]
+    weight_num = weight[np.logical_not(cat_features)]
 
     out = np.zeros((x_n_rows, y_n_rows), dtype=np.float32)
 
@@ -99,50 +96,47 @@ def gower_matrix(data_x, data_y=None, weight=None, cat_features=None):
     Y_cat = Z_cat[y_index,]
     Y_num = Z_num[y_index,]
 
-   # print(X_cat,X_num,Y_cat,Y_num)
-
     g = partial(f, x_n_rows=x_n_rows, y_n_rows=y_n_rows, X_cat=X_cat, X_num=X_num, Y_cat=Y_cat, Y_num=Y_num,
                 weight_cat=weight_cat, weight_num=weight_num, weight_sum=weight_sum, num_ranges=num_ranges)
     for i, res in enumerate(process_map(g, range(x_n_rows), chunksize=10)):
-        j_start= i
+        j_start = i
         if x_n_rows != y_n_rows:
             j_start = 0
-        out[i,j_start:]=res
-        if x_n_rows == y_n_rows: out[i:,j_start]=res
+        out[i, j_start:] = res
+        if x_n_rows == y_n_rows:
+            out[i:, j_start] = res
 
     return out
 
 
-def gower_get(xi_cat,xi_num,xj_cat,xj_num,feature_weight_cat,
-              feature_weight_num,feature_weight_sum,ranges_of_numeric):
-
+def gower_get(xi_cat, xi_num, xj_cat, xj_num, feature_weight_cat,
+              feature_weight_num, feature_weight_sum, ranges_of_numeric):
     # categorical columns
-    sij_cat = np.where(xi_cat == xj_cat,np.zeros_like(xi_cat),np.ones_like(xi_cat))
-    sum_cat = np.multiply(feature_weight_cat,sij_cat).sum(axis=1)
+    sij_cat = np.where(xi_cat == xj_cat, np.zeros_like(xi_cat), np.ones_like(xi_cat))
+    sum_cat = np.multiply(feature_weight_cat, sij_cat).sum(axis=1)
 
     # numerical columns
-    abs_delta=np.absolute(xi_num-xj_num)
-    sij_num=np.divide(abs_delta, ranges_of_numeric, out=np.zeros_like(abs_delta), where=ranges_of_numeric!=0)
+    abs_delta = np.absolute(xi_num - xj_num)
+    sij_num = np.divide(abs_delta, ranges_of_numeric, out=np.zeros_like(abs_delta), where=ranges_of_numeric != 0)
 
-    sum_num = np.multiply(feature_weight_num,sij_num).sum(axis=1)
-    sums= np.add(sum_cat,sum_num)
-    sum_sij = np.divide(sums,feature_weight_sum)
+    sum_num = np.multiply(feature_weight_num, sij_num).sum(axis=1)
+    sums = np.add(sum_cat, sum_num)
+    sum_sij = np.divide(sums, feature_weight_sum)
 
     return sum_sij
 
+
 def smallest_indices(ary, n):
     """Returns the n largest indices from a numpy array."""
-    #n += 1
     flat = np.nan_to_num(ary.flatten(), nan=999)
     indices = np.argpartition(-flat, -n)[-n:]
     indices = indices[np.argsort(flat[indices])]
-    #indices = np.delete(indices,0,0)
     values = flat[indices]
     return {'index': indices, 'values': values}
 
-def gower_topn(data_x, data_y=None, weight=None, cat_features=None, n = 5):
 
+def gower_topn(data_x, data_y=None, weight=None, cat_features=None, n=5):
     if data_x.shape[0] >= 2: TypeError("Only support `data_x` of 1 row. ")
     dm = gower_matrix(data_x, data_y, weight, cat_features)
 
-    return smallest_indices(np.nan_to_num(dm[0], nan=1),n)
+    return smallest_indices(np.nan_to_num(dm[0], nan=1), n)
