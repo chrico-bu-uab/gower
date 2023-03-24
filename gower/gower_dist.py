corrected h_t calculation

gower/gower_dist.py
@@ -132,7 +132,7 @@ def gower_matrix(data_x, data_y=None, weight=None, cat_features=None, R=(0, 100)
     if c > 0:
         p0, p1 = get_percentiles(X_num, R)
         dist = norm(0, 1)
-        h_t = c * x_n_rows ** 0.2 * np.minimum(np.nanstd(X_num, axis=0), (p1 - p0) / (dist.ppf(p1) - dist.ppf(p0)))
+        h_t = c * x_n_rows ** -0.2 * np.minimum(np.nanstd(X_num, axis=0), (p1 - p0) / (dist.ppf(p1) - dist.ppf(p0)))
     g = partial(f, x_n_rows=x_n_rows, y_n_rows=y_n_rows, X_cat=X_cat, X_num=X_num, Y_cat=Y_cat, Y_num=Y_num,
                 weight_cat=weight_cat, weight_num=weight_num, weight_sum=weight_sum, num_ranges=num_ranges, h_t=h_t,
                 knn_models=knn_models)

tests/matrix_test.py
@@ -4,13 +4,15 @@ import pytest
 
 import gower
 
-from sklearn.cluster import DBSCAN
+from sklearn.mixture import GaussianMixture
 
 np.set_printoptions(precision=0, suppress=True)
 pd.set_option('display.max_columns', None)
 
 
 def test_answer():
+    print()
+
     Xd = pd.DataFrame({'age': [21, 21, 19, 30, 21, 21, 19, 30, None],
                        'gender': ['M', 'M', 'N', 'M', 'F', 'F', 'F', 'F', None],
                        'civil_status': ['MARRIED', 'SINGLE', 'SINGLE', 'SINGLE', 'MARRIED', 'SINGLE', 'WIDOW',
@@ -19,28 +21,32 @@ def test_answer():
                        'has_children': [1, 0, 1, 1, 1, 0, 0, 1, None],
                        'available_credit': [2200, 100, 22000, 1100, 2000, 100, 6000, 2200, None]})
     X = np.asarray(Xd)
-    Xd['a'] = None
-    Xd['b'] = None
-    Xd['c'] = None
-    Xd['d'] = None
+    Xd['uniform'] = None
+    Xd['auto'] = None
+    Xd['h_t'] = None
+    Xd['knn'] = None
 
-    dbscan = DBSCAN(eps=0.13, min_samples=1, metric="precomputed")
+    model = GaussianMixture(n_components=4, random_state=0)
 
     aaa = gower.gower_matrix(X, weight=np.ones(6))
     assert aaa[0][1] == pytest.approx(0.3590238094329834), aaa[0][1]
-    Xd.iloc[:-1, -4] = dbscan.fit_predict(aaa[:-1, :-1])
+    Xd.iloc[:-1, -4] = model.fit_predict(aaa[:-1, :-1])
+    print(np.nanmean(aaa), np.nanstd(aaa))
 
     aaa = gower.gower_matrix(X)
     assert aaa[0][1] == pytest.approx(0.2916707939830703), aaa[0][1]
-    Xd.iloc[:-1, -3] = dbscan.fit_predict(aaa[:-1, :-1])
+    Xd.iloc[:-1, -3] = model.fit_predict(aaa[:-1, :-1])
+    print(np.nanmean(aaa), np.nanstd(aaa))
 
     aaa = gower.gower_matrix(X, R=(30, 70), c=2)
-    assert aaa[0][1] == pytest.approx(0.445336398682882), aaa[0][1]
-    Xd.iloc[:-1, -2] = dbscan.fit_predict(aaa[:-1, :-1])
+    assert aaa[0][1] == pytest.approx(0.5548674556378921), aaa[0][1]
+    Xd.iloc[:-1, -2] = model.fit_predict(aaa[:-1, :-1])
+    print(np.nanmean(aaa), np.nanstd(aaa))
 
     aaa = gower.gower_matrix(X, knn=True)
     assert aaa[0][1] == pytest.approx(0.12500412731640362), aaa[0][1]
-    Xd.iloc[:-1, -1] = dbscan.fit_predict(aaa[:-1, :-1])
+    Xd.iloc[:-1, -1] = model.fit_predict(aaa[:-1, :-1])
+    print(np.nanmean(aaa), np.nanstd(aaa))
 
-    print()
-    print(Xd)
+    print(Xd.iloc[:, :6])
+    print(Xd.iloc[:, 6:])