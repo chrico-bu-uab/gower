922e12d719f71e8bce5b821dbc002b1788761cfa
Chris Coffee <chrico@uab.edu>
2023-10-09 21:31:58 -0500

    made davies-boulding l1

--- gower/gower_dist.py
+++ gower/gower_dist.py
@@ -18,18 +18,21 @@ from sklearn.metrics import (
     adjusted_mutual_info_score,
     adjusted_rand_score,
     silhouette_score,
-    davies_bouldin_score,
-    calinski_harabasz_score,
+    # davies_bouldin_score,
+    calinski_harabasz_score, pairwise_distances,
 )
+from sklearn.metrics.cluster._unsupervised import check_number_of_labels
 from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import check_X_y, _safe_indexing, validate_params
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
 
 # Forked from https://pypi.org/project/gower/
 
-# Everything in this package uses Manhattan distance (except DB score)! :)
+# Everything in this package uses Manhattan distance (except CH score)! :)
 
 
 def get_cat_features(X):
@@ -378,6 +381,75 @@ def hamming_similarity(df):
 
 
 # Clustering Metrics
+
+
+@validate_params(
+    {
+        "X": ["array-like"],
+        "labels": ["array-like"],
+    },
+    prefer_skip_nested_validation=True,
+)
+def davies_bouldin_score(X, labels, **kwargs):
+    """Compute the Davies-Bouldin score.
+
+    The score is defined as the average similarity measure of each cluster with
+    its most similar cluster, where similarity is the ratio of within-cluster
+    distances to between-cluster distances. Thus, clusters which are farther
+    apart and less dispersed will result in a better score.
+
+    The minimum score is zero, with lower values indicating better clustering.
+
+    Read more in the :ref:`User Guide <davies-bouldin_index>`.
+
+    .. versionadded:: 0.20
+
+    Parameters
+    ----------
+    X : array-like of shape (n_samples, n_features)
+        A list of ``n_features``-dimensional data points. Each row corresponds
+        to a single data point.
+
+    labels : array-like of shape (n_samples,)
+        Predicted labels for each sample.
+
+    Returns
+    -------
+    score: float
+        The resulting Davies-Bouldin score.
+
+    References
+    ----------
+    .. [1] Davies, David L.; Bouldin, Donald W. (1979).
+       `"A Cluster Separation Measure"
+       <https://ieeexplore.ieee.org/document/4766909>`__.
+       IEEE Transactions on Pattern Analysis and Machine Intelligence.
+       PAMI-1 (2): 224-227
+    """
+    X, labels = check_X_y(X, labels)
+    le = LabelEncoder()
+    labels = le.fit_transform(labels)
+    n_samples, _ = X.shape
+    n_labels = len(le.classes_)
+    check_number_of_labels(n_labels, n_samples)
+
+    intra_dists = np.zeros(n_labels)
+    centroids = np.zeros((n_labels, len(X[0])), dtype=float)
+    for k in range(n_labels):
+        cluster_k = _safe_indexing(X, labels == k)
+        centroid = cluster_k.mean(axis=0)
+        centroids[k] = centroid
+        intra_dists[k] = np.average(pairwise_distances(cluster_k, [centroid], **kwargs))
+
+    centroid_distances = pairwise_distances(centroids, **kwargs)
+
+    if np.allclose(intra_dists, 0) or np.allclose(centroid_distances, 0):
+        return 0.0
+
+    centroid_distances[centroid_distances == 0] = np.inf
+    combined_intra_dists = intra_dists[:, None] + intra_dists
+    scores = np.max(combined_intra_dists / centroid_distances, axis=1)
+    return np.mean(scores)
 
 
 def dunn(X, **kwargs):
@@ -828,7 +900,7 @@ def evaluate_clusters(sample, matrix, actual: pd.Series, method, precomputed):
     # NOTE: These metrics are not supported per se for precomputed distances,
     # but they appear to work fine.
     try:
-        db = davies_bouldin_score(matrix, clusters)
+        db = davies_bouldin_score(matrix, clusters, metric="minkowski", p=1)
     except ValueError:
         db = np.nan
     try:
